1407fe07296d7b2740912e3c01a48aa8264f9b7e
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-10-23 15:12:53 -0600

    Removed Struct

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,22 +1,25 @@
 DISPLACEMENTS = [
   [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]
 ]
-Square = Struct.new(:x, :y) do
-  def to_s
-    column = ('a'.ord + x).chr
-    row = y + 1
 
-    "#{column}#{row}"
-  end
+def inside_board?(square)
+  (0..7).cover?(square[0]) && (0..7).cover?(square[1])
+end
+
+def algebraic(square)
+  column = ('a'.ord + square[0]).chr
+  row = square[1] + 1
+
+  "#{column}#{row}"
 end
 
 def jumps(square)
   DISPLACEMENTS.filter_map do |dx, dy|
-    x = square.x + dx
-    y = square.y + dy
-    Square.new(x, y) if (0..7).cover?(x) && (0..7).cover?(y)
+    jump = [square[0] + dx, square[1] + dy]
+    jump if inside_board?(jump)
   end
 end
 
 def knight_moves(origin, target)
+  
 end
