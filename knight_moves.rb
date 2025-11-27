671039181aa7f7d9bfa89dec949863dd15ce4ed5
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-11-27 14:52:52 -0600

    Missing BFS approach

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,9 +1,11 @@
+require 'pry-byebug'
+
 DISPLACEMENTS = [
   [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]
 ]
 
 def inside_board?(square)
-  (0..7).cover?(square[0]) && (0..7).cover?(square[1])
+  square.all? { |coord| coord.between?(0, 7) }
 end
 
 def algebraic(square)
@@ -13,13 +15,28 @@ def algebraic(square)
   "#{column}#{row}"
 end
 
-def jumps(square)
+def jumps(square, avoid = [])
   DISPLACEMENTS.filter_map do |dx, dy|
     jump = [square[0] + dx, square[1] + dy]
-    jump if inside_board?(jump)
+    jump if inside_board?(jump) && !avoid.include?(square)
   end
 end
 
 def knight_moves(origin, target)
-  
+  return if [origin, target].any? { |square| !inside_board?(square) }
+
+  queue = [origin]
+  visited = []
+
+  while queue
+    available_jumps = jumps(queue[0], visited)
+    visited << queue.shift
+    #puts "Available jumps: #{available_jumps.size}"
+    queue += available_jumps if !available_jumps.empty?
+    #puts "Current queue: #{queue.inspect}"
+    
+    break if visited.last == target
+  end
+
+  visited
 end
