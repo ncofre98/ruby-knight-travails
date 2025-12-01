92a0cd308052cdc1fc92bc199fb2d3c8c2868e36
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-11-30 22:01:38 -0600

    just in case, closest solution

--- knight_moves.rb
+++ knight_moves.rb
@@ -16,27 +16,34 @@ def algebraic(square)
 end
 
 def jumps(square, avoid = [])
+  return if !square
+  
   DISPLACEMENTS.filter_map do |dx, dy|
     jump = [square[0] + dx, square[1] + dy]
-    jump if inside_board?(jump) && !avoid.include?(square)
+    jump if inside_board?(jump) && !avoid.include?(jump)
   end
 end
 
 def knight_moves(origin, target)
   return if [origin, target].any? { |square| !inside_board?(square) }
 
-  queue = [origin]
+  queue = [[origin]]
+  current_square = origin
   visited = []
+  
+  while current_square != target
+    current_path = queue.shift
+    current_square = current_path.last
+    visited << current_square
+    available_jumps = jumps(current_square, visited)
 
-  while queue
-    available_jumps = jumps(queue[0], visited)
-    visited << queue.shift
-    #puts "Available jumps: #{available_jumps.size}"
-    queue += available_jumps if !available_jumps.empty?
-    #puts "Current queue: #{queue.inspect}"
-    
-    break if visited.last == target
+    if available_jumps && !available_jumps.empty?
+      available_jumps.each do |new_square|
+        binding.pry
+        queue << (current_path + [new_square])
+      end
+    end
   end
 
-  visited
+  queue
 end
